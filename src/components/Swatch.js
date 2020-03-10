e6e6cb185717db95fac986c90f67bc6512db9617
Elliot Winkler <elliot.winkler@gmail.com>
2020-03-09 20:23:53 -0600

    Draw/update gradiated RGB swatch using web workers

--- src/components/Swatch.js
+++ src/components/Swatch.js
@@ -1,20 +1,42 @@
-import React from "react";
-
-//import { COLOR_REPRESENTATION_NAMES } from "../lib/colorRepresentationsByName";
+import React, { useEffect, useLayoutEffect, useRef } from "react";
 
 import styles from "./Swatch.css";
+
+const SIZE = 200;
+const worker = new Worker("../worker.js");
 
 export default function Swatch({
   colorsByRepresentationName,
   lastColorUpdated
 }) {
+  const onscreenCanvasRef = useRef(null);
+  const offscreenCanvasRef = useRef(null);
+
+  useEffect(() => {
+    offscreenCanvasRef.current = onscreenCanvasRef.current.transferControlToOffscreen();
+    worker.postMessage(
+      {
+        message: "start",
+        canvas: offscreenCanvasRef.current,
+        canvasSize: SIZE
+      },
+      [offscreenCanvasRef.current]
+    );
+  }, []);
+
+  useEffect(() => {
+    worker.postMessage({
+      message: "draw",
+      color: lastColorUpdated.convertTo("rgb").toPlainObject()
+    });
+  }, [lastColorUpdated]);
+
   return (
-    <div
+    <canvas
+      width={SIZE}
+      height={SIZE}
+      ref={onscreenCanvasRef}
       className={styles.swatch}
-      style={{
-        backgroundColor: lastColorUpdated.toFormattedString({ hex: true }),
-        color: lastColorUpdated.textColor
-      }}
     />
   );
 }
---
+++ src/worker.js
@@ -0,0 +1,36 @@
+function setPixelOn(imageData, { x, y }, { r, g, b, a }) {
+  const index = (x + y * imageData.width) * 4;
+  imageData.data[index + 0] = r;
+  imageData.data[index + 1] = g;
+  imageData.data[index + 2] = b;
+  imageData.data[index + 3] = a;
+}
+
+self.hasStarted = false;
+
+self.onmessage = event => {
+  switch (event.data.message) {
+    case "start":
+      self.hasStarted = true;
+      self.canvas = event.data.canvas;
+      self.canvasSize = event.data.canvasSize;
+      break;
+    case "draw":
+      const color = event.data.color;
+      const ratio = 255 / self.canvasSize;
+      const ctx = self.canvas.getContext("2d");
+      const imageData = ctx.createImageData(self.canvasSize, self.canvasSize);
+      for (let y = 0; y < self.canvasSize; y++) {
+        for (let x = 0; x < self.canvasSize; x++) {
+          const g = x * ratio;
+          const b = y * ratio;
+          setPixelOn(imageData, { x, y }, { r: color.r, g: g, b: b, a: 255 });
+        }
+      }
+      ctx.putImageData(imageData, 0, 0);
+      break;
+    default:
+      throw new Error(`Unknown message ${event.data.message}!`);
+      break;
+  }
+};
